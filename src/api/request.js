cd50182e85c8c1d9f56400825fb1adafed83ad50
KT <KT@T14.local>
2019-07-10 02:46:10 +0900

    fix error handling

--- src/api/request.js
+++ src/api/request.js
@@ -5,18 +5,27 @@
  */
 export default function request(conditions, cb) {
   let url = new URL("https://api.gnavi.co.jp/RestSearchAPI/v3/");
-  const params = {keyid: process.env.REACT_APP_GNAVI_ACCESS_KEY, ...conditions};
+  const params = {
+    keyid: process.env.REACT_APP_GNAVI_ACCESS_KEY,
+    ...conditions
+  };
   Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
   fetch(url)
     .then(response => {
-      if (!response.ok) throw new Error("request not ok.");
-      else return response.json();
+      // if (!response.ok)
+      //   throw new Error("request not ok.");
+      // else
+      return response.json();
     })
-    .then(json => cb(json))
+    .then(json => {
+      console.log(JSON.stringify(json));
+      if (json.error) {
+        throw new Error(json.error[0].message);
+      }
+      return cb(json);
+    })
     .catch(err => {
       console.log(err);
-      alert(
-        "ネットワークエラーが発生しました。\nしばらく経ってからもう一度試してください。"
-      );
+      alert(err.message);
     });
 }
